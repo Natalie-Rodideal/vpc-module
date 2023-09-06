dddbd12fdcbf10fb8d852056f29fc3903d3b0cae
Natalia R <78937745+Natalie-Rodideal@users.noreply.github.com>
2023-09-06 10:33:56 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,82 @@
 
+variable "region" {
+  description = "Region in which AWS Resources to be created"
+  type        = string
+  default     = "us-east-1"
+}
+# 
+variable "accountname" {
+  type    = string
+  default = ""
+
+}
+
+variable "vpc_name" {
+  description = "VPC Name"
+  type        = string
+  default     = ""
+}
+
+# VPC CIDR Block
+variable "cidr" {
+  description = "VPC CIDR Block"
+  type        = string
+  default     = "10.164.32.0/22"
+}
+
+variable "approver" {
+  type    = string
+  default = ""
+
+}
+variable "Created_By" {
+  type    = string
+  default = "terraform"
+}
+# Environment Variable
+variable "environment" {
+  description = "Environment Variable used as a prefix"
+  type        = string
+  default     = "dev"
+}
+variable "business-unit" {
+  description = "Business Division in the large organization this Infrastructure belongs"
+  type        = string
+  default     = "DevOps"
+}
+variable "costcenter" {
+  type    = string
+  default = "000000"
+}
+variable "owner" {
+  type    = string
+  default = ""
+}
+variable "Terraform_Managed" {
+  type    = bool
+  default = "true"
+}
+variable "private_subnet1" {
+  type    = string
+  default = ""
+}
+variable "private_subnet2" {
+  type    = string
+  default = ""
+}
+variable "public_subnet1" {
+  type    = string
+  default = ""
+}
+variable "public_subnet2" {
+  type    = string
+  default = ""
+}
+
+variable "tgw_id" {
+  description = "ID for the Transit Gateway US-EAST-01"
+  default     = "tgw"
+}
+
+
+
